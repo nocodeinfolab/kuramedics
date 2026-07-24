47b771a69b7aeb8c5c12ede45239c0a6f4f4ae83
Chikezie Abia <nocodeinfolab@gmail.com>
2026-07-24 08:59:06 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,11 +3,14 @@ import { registerRoute, startRouter } from "./core/router.js";
 import { LandingPage } from "./components/landing/LandingPage.js";
 import { PatientLoginPage } from "./components/auth/PatientLoginPage.js";
 import { DoctorLoginPage } from "./components/auth/DoctorLoginPage.js";
+import DoctorDashboardPage from "./components/doctor/DoctorDashboardPage.js";
 
 registerRoute("/", () => new LandingPage());
 
 registerRoute("/patient/login", () => new PatientLoginPage());
 
 registerRoute("/doctor/login", () => new DoctorLoginPage());
+
+registerRoute("/doctor/dashboard", () => new DoctorDashboardPage());
 
 startRouter();
