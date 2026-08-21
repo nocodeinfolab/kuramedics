0fc27acfc2b84ff3ff50561fa9f13f719561b0e3
Chikezie Abia <nocodeinfolab@gmail.com>
2026-08-21 20:38:37 +0100

    Add route for public doctor profile page

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ import { DoctorLoginPage } from "./components/auth/DoctorLoginPage.js";
 import DoctorDashboardPage from "./components/doctor/DoctorDashboardPage.js";
 import PatientDashboardPage from "./components/patient/PatientDashboardPage.js";
 import PrescriptionVerificationPage from "./components/public/PrescriptionVerificationPage.js";
+import PublicDoctorProfilePage from "./components/public/PublicDoctorProfilePage.js";
 
 registerRoute("/", () => new LandingPage());
 
@@ -16,5 +17,6 @@ registerRoute("/doctor/login", () => new DoctorLoginPage());
 registerRoute("/doctor/dashboard", () => new DoctorDashboardPage());
 registerRoute("/patient/dashboard", () => new PatientDashboardPage());
 registerRoute("/verify/prescription/:id", (params) => new PrescriptionVerificationPage(params.id));
+registerRoute("/doctor/:id", (params) => new PublicDoctorProfilePage(params.id));
 
 startRouter();
