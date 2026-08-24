3424de9c37e35a1933de3b17512883e493f7e037
Chikezie Abia <nocodeinfolab@gmail.com>
2026-08-25 00:13:40 +0100

    Update AdminDashboardPage.js

--- js/components/admin/AdminDashboardPage.js
+++ js/components/admin/AdminDashboardPage.js
@@ -4,6 +4,7 @@ import AdminOverview from "./AdminOverview.js";
 import AdminVerifications from "./AdminVerifications.js";
 import AdminUsers from "./AdminUsers.js";
 import AdminSubscriptionSettings from "./AdminSubscriptionSettings.js";
+import AdminUpdates from "./AdminUpdates.js";
 
 const Icons = {
     grid: () => h("svg", { width: "18", height: "18", viewBox: "0 0 24 24", fill: "none", stroke: "currentColor", "stroke-width": "2", "stroke-linecap": "round", "stroke-linejoin": "round" },
@@ -19,6 +20,8 @@ const Icons = {
         h("path", { d: "M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 1 1-2.83 2.83l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-4 0v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 1 1-2.83-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1 0-4h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 1 1 2.83-2.83l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 4 0v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 1 1 2.83 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 0 4h-.09a1.65 1.65 0 0 0-1.51 1z" })),
     logout: () => h("svg", { width: "18", height: "18", viewBox: "0 0 24 24", fill: "none", stroke: "currentColor", "stroke-width": "2", "stroke-linecap": "round", "stroke-linejoin": "round" },
         h("path", { d: "M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" }), h("polyline", { points: "16 17 21 12 16 7" }), h("line", { x1: "21", y1: "12", x2: "9", y2: "12" })),
+    upload: () => h("svg", { width: "18", height: "18", viewBox: "0 0 24 24", fill: "none", stroke: "currentColor", "stroke-width": "2", "stroke-linecap": "round", "stroke-linejoin": "round" },
+        h("path", { d: "M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" }), h("polyline", { points: "17 8 12 3 7 8" }), h("line", { x1: "12", y1: "3", x2: "12", y2: "15" })),
 };
 
 export default class AdminDashboardPage extends Component {
@@ -37,6 +40,7 @@ export default class AdminDashboardPage extends Component {
             { id: "verifications", label: "Verifications", icon: Icons.shield, badgeKey: "pendingCount" },
             { id: "users", label: "Users", icon: Icons.people },
             { id: "subscription", label: "Subscription Settings", icon: Icons.settings },
+            { id: "updates", label: "App Updates", icon: Icons.upload },
         ];
     }
 
@@ -210,6 +214,9 @@ export default class AdminDashboardPage extends Component {
                 break;
             case "subscription":
                 instance = new AdminSubscriptionSettings();
+                break;
+            case "updates":
+                instance = new AdminUpdates();
                 break;
             default:
                 instance = new AdminOverview();
