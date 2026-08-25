b496f2d99641131f51c723505dd21e556d77a77d
Chikezie Abia <nocodeinfolab@gmail.com>
2026-08-25 23:38:46 +0100

    Update PatientCare.js

--- js/components/patient/PatientCare.js
+++ js/components/patient/PatientCare.js
@@ -248,7 +248,7 @@ export default class PatientCare extends Component {
     renderHeader() {
         return h(
             "section",
-            
+            { class: "dashboard-header dashboard-header--tab" },  // ← Add new class
             h("p", { class: "dashboard-greeting" }, "My Care"),
             h("h1", { class: "dashboard-title" }, "Appointments & Records"),
             h(
