3fa8fdaaf42057de480c98c60bff9770aa3b61c4
Chikezie Abia <nocodeinfolab@gmail.com>
2026-09-01 23:55:00 +0100

    Update DoctorLoginPage.js

--- js/components/auth/DoctorLoginPage.js
+++ js/components/auth/DoctorLoginPage.js
@@ -87,7 +87,20 @@ export class DoctorLoginPage extends Component {
           disabled: true,
           title: "Apple Sign In coming soon"
         },
-        h("span", { class: "apple-signin-icon" }, ""),
+        h(
+          "svg",
+          {
+            class: "apple-signin-icon",
+            viewBox: "0 0 24 24",
+            width: "20",
+            height: "20",
+            "aria-hidden": "true"
+          },
+          h("path", {
+            fill: "currentColor",
+            d: "M17.05 20.28c-.98.95-2.05.8-3.08.35-1.09-.46-2.09-.48-3.24 0-1.44.62-2.2.44-3.06-.35C2.79 15.25 3.51 7.59 9.05 7.31c1.35.07 2.29.74 3.08.8 1.18-.24 2.31-.93 3.57-.84 1.51.12 2.65.72 3.39 1.8-3.12 1.87-2.38 5.98.48 7.13-.57 1.5-1.31 2.95-2.52 4.09zM12.03 7.25C11.88 5.02 13.69 3.18 15.76 3c.29 2.58-2.34 4.5-3.73 4.25z"
+          })
+        ),
         h("span", {}, "Sign in with Apple")
       ),
   
